f5073d744d75daa0e5c62f0c8618c9ebb0407fc1
rayanBendjeddoub22002597 <rayan.bendjeddou@etu.univ-amu.fr>
2023-06-06 22:09:52 +0200

    Création de la fenêtre (à partir du schéma réalisé)

--- src/main/java/fr/amu/iut/sismicviewer/scenes/carte/CarteController.java
+++ src/main/java/fr/amu/iut/sismicviewer/scenes/carte/CarteController.java
@@ -1,9 +1,15 @@
 package fr.amu.iut.sismicviewer.scenes.carte;
 
+import com.gluonhq.maps.MapLayer;
+import com.gluonhq.maps.MapPoint;
+import com.gluonhq.maps.MapView;
+import fr.amu.iut.sismicviewer.Gluon.CustomCircleMarkerLayer;
 import fr.amu.iut.sismicviewer.controllers.TopBarController;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.input.ScrollEvent;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -19,9 +25,24 @@ public class CarteController implements Initializable {
     @FXML
     private Button stats;
 
+    @FXML
+    private MapView mapView;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         TopBarController topBarController = new TopBarController();
         topBarController.initTopBar(carte, dashboard, stats);
+        initMap();
+    }
+
+    /* Initialise la map */
+    public void initMap(){
+        mapView.addEventFilter(MouseEvent.ANY, event -> event.consume());
+        mapView.addEventFilter(ScrollEvent.ANY, event -> event.consume());
+        MapPoint mapPoint = new MapPoint(46.727638, 2.213749);
+        mapView.setZoom(5.1);
+        mapView.flyTo(0, mapPoint, 0.1);
+        MapLayer mapLayer = new CustomCircleMarkerLayer(mapPoint);
+        mapView.addLayer(mapLayer);
     }
 }
