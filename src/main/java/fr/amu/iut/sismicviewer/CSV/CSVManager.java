6baa6b01704039eb93f0b5cef54ec31a9683c47d
Mathis <mathis.louarn@etu.univ-amu.fr>
2023-06-06 15:04:05 +0200

    Ajout de méthodes dans le CSVManager getData() getData(int) getData(int,int) getListeAttributs()

--- src/main/java/fr/amu/iut/sismicviewer/CSV/CSVManager.java
+++ src/main/java/fr/amu/iut/sismicviewer/CSV/CSVManager.java
@@ -22,7 +22,7 @@ public class CSVManager {
                 data.add(nextRecord);
                 ++nombre_de_donnees;
             }
-            nombre_attributs = nextRecord.length;
+            nombre_attributs = data.get(0).length;
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -47,5 +47,42 @@ public class CSVManager {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public String[] getData(int ligne) {
+        if (ligne > nombre_de_donnees) {
+            throw new IllegalArgumentException("L'indice spécifié dépasse le nombre d'entrée dans le fichier CSV");
+        } else if (ligne < 1) {
+            throw new IllegalArgumentException("L'indice ne peut être négatif");
+        }
+        for (String donnee : data.get((ligne))) {
+            System.out.print(donnee + " ; ");
+        }
+        System.out.println();
+        return data.get(ligne);
+    }
+
+    public ArrayList<String[]> getData(int ligne_dep, int ligne_fin) {
+        ArrayList<String[]> data_coupe = new ArrayList<String[]>();
+        for (int i = ligne_dep; i < ligne_fin; ++i) {
+            data_coupe.add(this.getData(i));
+        }
+        return data_coupe;
+    }
+
+    public ArrayList<String[]> getData() {
+        return getData(1, nombre_de_donnees);
+    }
+
+    public int getNombre_de_donnees() {
+        return nombre_de_donnees - 1;
+    }
+
+    public int getNombre_attributs() {
+        return nombre_attributs;
+    }
+
+    public String[] getListeAttributs() {
+        return data.get(0);
     }
 }
