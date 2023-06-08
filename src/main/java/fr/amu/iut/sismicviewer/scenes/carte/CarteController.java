b546a0e94543f7254fe2b17b629e1b1fa0af9df6
rayanBendjeddoub22002597 <rayan.bendjeddou@etu.univ-amu.fr>
2023-06-08 09:56:55 +0200

    Ajout colonnes dans la tableview (pour les futurs données)

--- src/main/java/fr/amu/iut/sismicviewer/scenes/carte/CarteController.java
+++ src/main/java/fr/amu/iut/sismicviewer/scenes/carte/CarteController.java
@@ -5,10 +5,17 @@ import com.gluonhq.maps.MapPoint;
 import com.gluonhq.maps.MapView;
 import fr.amu.iut.sismicviewer.Gluon.CustomCircleMarkerLayer;
 import fr.amu.iut.sismicviewer.controllers.TopBarController;
+import javafx.beans.InvalidationListener;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.input.ScrollEvent;
@@ -20,24 +27,32 @@ public class CarteController implements Initializable {
 
     @FXML
     private Button carte;
-
     @FXML
     private Button dashboard;
-
     @FXML
     private Button stats;
-
     @FXML
     private MapView mapView;
-
     @FXML
     private TableView tableView;
+    @FXML
+    private TableColumn region;
+    @FXML
+    private TableColumn intensite;
+    @FXML
+    private TableColumn qie;
+    @FXML
+    private TableColumn choc;
+    @FXML
+    private TableColumn date;
+
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         TopBarController topBarController = new TopBarController();
         topBarController.initTopBar(carte, dashboard, stats);
         initMap();
+        System.out.println("Initialisation du contrôleur de carte..");
     }
 
     /* Initialise la map */
