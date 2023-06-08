a2cd2986985fea1be4826df563366b5e5d42e341
rayanBendjeddoub22002597 <rayan.bendjeddou@etu.univ-amu.fr>
2023-06-08 18:36:38 +0200

    Importation des données possible (doit-prendre en charge les erreurs)

--- src/main/java/fr/amu/iut/sismicviewer/scenes/carte/CarteController.java
+++ src/main/java/fr/amu/iut/sismicviewer/scenes/carte/CarteController.java
@@ -3,23 +3,33 @@ package fr.amu.iut.sismicviewer.scenes.carte;
 import com.gluonhq.maps.MapLayer;
 import com.gluonhq.maps.MapPoint;
 import com.gluonhq.maps.MapView;
+import fr.amu.iut.sismicviewer.CSV.CSVManager;
+import fr.amu.iut.sismicviewer.CSV.SeismeDataManager;
+import fr.amu.iut.sismicviewer.Seisme;
+import fr.amu.iut.sismicviewer.SismicViewerApp;
 import fr.amu.iut.sismicviewer.controllers.TopBarController;
+import fr.amu.iut.sismicviewer.scenes.dashboard.TopSeismeControl;
 import javafx.beans.InvalidationListener;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.input.ScrollEvent;
 
+import java.io.File;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class CarteController implements Initializable {
@@ -35,22 +45,31 @@ public class CarteController implements Initializable {
     @FXML
     private TableView tableView;
     @FXML
-    private TableColumn region;
+    private TableColumn lieu;
     @FXML
     private TableColumn intensite;
     @FXML
-    private TableColumn qie;
+    private TableColumn latitude;
     @FXML
-    private TableColumn choc;
+    private TableColumn longitude;
     @FXML
-    private TableColumn date;
+    private TableColumn annee;
+    @FXML
+    private Label msgErreur;
+
+    private File csvFile;
 
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         TopBarController topBarController = new TopBarController();
+        SeismeDataManager seismeDataManager = new SeismeDataManager();
         topBarController.initTopBar(carte, dashboard, stats);
         initMap();
+        initTableView();
+
+
+
         System.out.println("Initialisation du contrôleur de carte..");
     }
 
@@ -61,5 +80,12 @@ public class CarteController implements Initializable {
         MapPoint mapPoint = new MapPoint(46.727638, 2.213749);
         mapView.setZoom(5.8);
         mapView.flyTo(0, mapPoint, 0.1);
+    }
+
+    public void initTableView() {
+        tableView.getSelectionModel().setCellSelectionEnabled(true);
+        CSVManager csvManager = new CSVManager();
+        csvManager.loadCsv(SismicViewerApp.getCsvFile());
+        tableView.setItems(FXCollections.observableList(csvManager.getListeSeisme()));
     }
 }
