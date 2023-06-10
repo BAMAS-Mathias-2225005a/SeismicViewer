7e79a267b75f0eb670b66d0466b7bde169d9777b
mathiasbamas <mathias.bamas@etu.univ-amu.fr>
2023-06-11 00:21:17 +0200

    Update MainMapLayer Ajout couleur

--- src/main/java/fr/amu/iut/sismicviewer/Gluon/MainMapLayer.java
+++ src/main/java/fr/amu/iut/sismicviewer/Gluon/MainMapLayer.java
@@ -3,6 +3,7 @@ package fr.amu.iut.sismicviewer.Gluon;
 import com.gluonhq.maps.MapLayer;
 import com.gluonhq.maps.MapPoint;
 import fr.amu.iut.sismicviewer.Seisme;
+import javafx.beans.binding.Binding;
 import javafx.geometry.Point2D;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
@@ -16,9 +17,26 @@ public class MainMapLayer extends MapLayer {
         for(Seisme seisme : listeSeismes){
             MapPoint point = new MapPoint(seisme.getLatitude(), seisme.getLongitude());
             Point2D point2D = getMapPoint(point.getLatitude(),point.getLongitude());
-            Circle circle = new Circle(5, Color.RED);
+            Circle circle = new Circle(5);
             circle.setCenterX(point2D.getX());
             circle.setCenterY(point2D.getY());
+
+            if(seisme.getMagnitude() < 3.5) {
+                circle.setFill(Color.GREEN);
+                circle.setRadius(2);
+            }else if(seisme.getMagnitude() < 5.5) {
+                circle.setFill(Color.YELLOW);
+                circle.setRadius(4);
+            }
+            else if(seisme.getMagnitude() < 7.5) {
+                circle.setFill(Color.ORANGE);
+                circle.setRadius(6);
+            }
+            else {
+                circle.setFill(Color.RED);
+                circle.setRadius(8);
+            }
+
             this.getChildren().add(circle);
         }
     }
